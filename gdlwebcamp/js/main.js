031659deccf3ffc8293fd27986756a3654df6d1e
Taro Rodriguez <lautam.rodriguez@gmail.com>
2019-12-02 18:09:43 -0300

    Update 2/12/19

--- gdlwebcamp/js/main.js
+++ gdlwebcamp/js/main.js
@@ -138,9 +138,57 @@
     }
   }); // DOM CONTENT LOADED
 })();
-
+//Menu seminarios
 $(function(){
- $('.menu-programa a').ON('click',function(){
-   var enlace=
- })
+  //Menu Fijo
+  var windowHeight = $(window).height();
+  var barraAltura = $('.barra').innerHeight();
+
+  $(window).scroll(function(){
+      var scroll=$(window).scrollTop();
+      if (scroll > windowHeight){
+        $('.barra').addClass('fixed');
+        $('body').css({'margin-top':barraAltura+'px'}) //quitar salto en la barra
+      }
+      else{
+        $('.barra').removeClass('fixed');
+        $('body').css({'margin-top':'0px'}) //quitar salto
+      }
+  });
+
+  //Menu responsive
+  $('.menu-movil').on ('click',function() {
+      $('.navegacion-principal').slideToggle();
+  });
+
+
+
+  //Lettering
+  $('.nombre-sitio').lettering();
+  //Programa de conferencias
+  $('.programa-evento .info-curso:first').show();
+  $('.menu-programa a:first').addClass('activo');
+
+  $('.menu-programa a').on('click',function(){
+    $('.menu-programa a').removeClass('activo');
+    $(this).addClass('activo');
+    $('.ocultar').hide();
+
+    var enlace=$(this).attr('href');
+    $(enlace).fadeIn(1000); //no olvidar mayusc. en  -> fadeIn <-
+    return false; //Para que no de saltos al clickar
+  })
+
+  // Animaciones para los numeros
+  $('.resumen-evento li:nth-child(1) p').animateNumber({number:6}, 1200);
+  $('.resumen-evento li:nth-child(1) p').animateNumber({number:15}, 1200);
+  $('.resumen-evento li:nth-child(1) p').animateNumber({number:3}, 1500);
+  $('.resumen-evento li:nth-child(1) p').animateNumber({number:9}, 1200);
+  // Cuenta regresiva
+  $('.cuenta-regresiva').countdown('2020/05/10 10:00:00', function(event){
+    $('#dias').html(event.strftime('%D'));
+    $('#horas').html(event.strftime('%H'));
+    $('#minutos').html(event.strftime('%M'));
+    $('#segundos').html(event.strftime('%S'));
+  })
 });
